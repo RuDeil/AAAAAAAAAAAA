6d0c43c0f97cadc116ec408970102f5113974f69
Михаил Крылов <Rudeil@yandex.ru>
2023-05-25 17:48:30 +0300

    asdsadsa

--- app/src/main/java/com/example/aaaaaaaaaaaa/MainActivity.kt
+++ app/src/main/java/com/example/aaaaaaaaaaaa/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "moskow,ru"
+    val API: String = "ea78610280af633a7578b4b1f22ee616"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
